526a52bd86ad695d238749ad3b810aab53c56a2a
Amar <ayadav42@asu.edu>
2022-03-16 00:24:29 -0700

    US#125 Task#132 Updated City Entity to accomodate for new cols

--- backend/server/seed/seed.py
+++ backend/server/seed/seed.py
@@ -25,7 +25,7 @@ def read_cities_and_states():
                 first = False
                 continue
 
-            city = City(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
+            city = City(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20], row[21], row[22], row[23], row[24], row[25], row[26], row[27], row[28], row[29], row[30], row[31])
             state = State(city)
             all_cities.add(city)
             all_states.add(state)
