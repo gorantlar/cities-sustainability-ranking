5c0cb7f1fb150b313a3ce080264ff0bec94bbc22
Amar <ayadav42@asu.edu>
2022-03-17 22:05:35 -0700

    US#97 Tasks 101,100 Added room for other data

--- backend/server/entities/City.py
+++ backend/server/entities/City.py
@@ -16,26 +16,26 @@ class City:
         self.density = density
         self.zips = zips
         self.speak_a_language_other_than_english = speak_a_language_other_than_english
-        self.bachelors_degree_or_higher = bachelors_degree_or_higher
-        self.two_or_more_races = two_or_more_races
-        self.park_access = park_access
-        self.walkability = walkability
-        self.limited_access_to_healthy_foods = limited_access_to_healthy_foods
-        self.foreign_born = foreign_born
-        self.state_annual_co2_equivalent_total_output_emission_rate_lb_mwh_2020 = state_annual_co2_equivalent_total_output_emission_rate_lb_mwh_2020
-        self.state_annual_total_nonrenewables_net_generation_mwh_2020 = state_annual_total_nonrenewables_net_generation_mwh_2020
-        self.state_annual_total_renewables_net_generation_mwh_2020 = state_annual_total_renewables_net_generation_mwh_2020
-        self.frequent_physical_distress_in_2018_percent = frequent_physical_distress_in_2018_percent
-        self.broadband_connection_2019 = broadband_connection_2019
-        self.park_access_2018 = park_access_2018
-        self.preventive_services_2018 = preventive_services_2018
-        self.walkability_2019 = walkability_2019
-        self.poplution_percent = poplution_percent
-        self.vacant_houses = vacant_houses
-        self.crimes_occurred = crimes_occurred
-        self.registered_voters = registered_voters
-        self.violent_crimes = violent_crimes
-        self.insurance = insurance
+        # self.bachelors_degree_or_higher = bachelors_degree_or_higher
+        # self.two_or_more_races = two_or_more_races
+        # self.park_access = park_access
+        # self.walkability = walkability
+        # self.limited_access_to_healthy_foods = limited_access_to_healthy_foods
+        # self.foreign_born = foreign_born
+        # self.state_annual_co2_equivalent_total_output_emission_rate_lb_mwh_2020 = state_annual_co2_equivalent_total_output_emission_rate_lb_mwh_2020
+        # self.state_annual_total_nonrenewables_net_generation_mwh_2020 = state_annual_total_nonrenewables_net_generation_mwh_2020
+        # self.state_annual_total_renewables_net_generation_mwh_2020 = state_annual_total_renewables_net_generation_mwh_2020
+        # self.frequent_physical_distress_in_2018_percent = frequent_physical_distress_in_2018_percent
+        # self.broadband_connection_2019 = broadband_connection_2019
+        # self.park_access_2018 = park_access_2018
+        # self.preventive_services_2018 = preventive_services_2018
+        # self.walkability_2019 = walkability_2019
+        # self.poplution_percent = poplution_percent
+        # self.vacant_houses = vacant_houses
+        # self.crimes_occurred = crimes_occurred
+        # self.registered_voters = registered_voters
+        # self.violent_crimes = violent_crimes
+        # self.insurance = insurance
 
     def get_city(self, node):
         if not isinstance(node, neo4j.graph.Node):
@@ -54,26 +54,26 @@ class City:
             node['density'],
             node['zips'],
             node['speak_a_language_other_than_english'],
-            node['bachelors_degree_or_higher']
-            node['two_or_more_races'],
-            node['park_access'],
-            node['walkability'],
-            node['limited_access_to_healthy_foods'],
-            node['foreign_born'],
-            node['state_annual_c_o_2_equivalent_total_output_emission_rate_lb_m_wh_2020'],
-            node['state_annual_total_nonrenewables_net_generation_m_wh_2020'],
-            node['state_annual_total_renewables_net_generation_m_wh_2020'],
-            node['frequent_physical_distress_in_2018_percent'],
-            node['broadband_connection_2019'],
-            node['park_access_2018'],
-            node['preventive_services_2018'],
-            node['walkability_2019'],
-            node['poplution_percent'],
-            node['vacant_houses'],
-            node['crimes_occurred'],
-            node['registered_voters'],
-            node['violent_crimes'],
-            node['insurance'],
+            # node['bachelors_degree_or_higher']
+            # node['two_or_more_races'],
+            # node['park_access'],
+            # node['walkability'],
+            # node['limited_access_to_healthy_foods'],
+            # node['foreign_born'],
+            # node['state_annual_c_o_2_equivalent_total_output_emission_rate_lb_m_wh_2020'],
+            # node['state_annual_total_nonrenewables_net_generation_m_wh_2020'],
+            # node['state_annual_total_renewables_net_generation_m_wh_2020'],
+            # node['frequent_physical_distress_in_2018_percent'],
+            # node['broadband_connection_2019'],
+            # node['park_access_2018'],
+            # node['preventive_services_2018'],
+            # node['walkability_2019'],
+            # node['poplution_percent'],
+            # node['vacant_houses'],
+            # node['crimes_occurred'],
+            # node['registered_voters'],
+            # node['violent_crimes'],
+            # node['insurance'],
         )
 
         return retVal
